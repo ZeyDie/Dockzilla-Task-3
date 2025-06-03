94105d26848ce6561eca56026d6e8428d6d4f960
ZeyDie <zeydie.dev@gmail.com>
2025-06-03 12:31:30 +0300

    Change port to 8090

--- src/main/java/pro/doczilla/common/CommonLaunch.java
+++ src/main/java/pro/doczilla/common/CommonLaunch.java
@@ -16,7 +16,7 @@ public class CommonLaunch implements ILaunch {
     private static final @NotNull CommonLaunch instance = new CommonLaunch();
 
     @Getter
-    private static final int port = 10000;
+    private static final int port = 8090;
 
     @Getter
     private static final @NotNull HttpClient httpClient = HttpClient.newBuilder()
