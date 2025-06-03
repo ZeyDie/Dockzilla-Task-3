e611bbf4a146c7d0a6a0e474812118c2c7f3b585
ZeyDie <zeydie.dev@gmail.com>
2025-06-03 12:29:45 +0300

    FIX port API

--- frontend/wearther/src/modules/api.ts
+++ frontend/wearther/src/modules/api.ts
@@ -1,7 +1,7 @@
 import {WeatherData} from './types';
 
 export async function fetchWeatherData(city: string): Promise<WeatherData> {
-    const url = `http://localhost:8080/weather?city=${encodeURIComponent(city)}`;
+    const url = `http://localhost:10000/weather?city=${encodeURIComponent(city)}`;
     const response = await fetch(url);
 
     if (!response.ok) {
